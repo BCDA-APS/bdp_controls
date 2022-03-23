60247ed8b6f9bbdc6c8cad3737e9d55c67b79f37
Pete R Jemian <prjemian@gmail.com>
2022-03-23 14:22:21 -0500

    MNT #10 simplify this API

--- qserver/instrument/plans/image_acquisition.py
+++ qserver/instrument/plans/image_acquisition.py
@@ -2,10 +2,7 @@
 Plans for the BPD 2022 project demonstration
 """
 
-__all__ = """
-    set_acquire_time
-    take_image
-""".split()
+__all__ = ["take_image", ]
 
 import logging
 logger = logging.getLogger(__name__)
@@ -32,9 +29,11 @@ PTIME_EXTRA = 0.001
 cat = databroker.catalog[iconfig["DATABROKER_CATALOG"]]
 
 
-def set_acquire_time(atime, aperiod=None):
+def take_image(atime, aperiod=None, md=None):
     """
-    Set the acquire time & period of the area detector.
+    Take one image with the area detector.
+
+    Hint:  If the shutter is not opened, the image will be background only.
 
     Parameters:
 
@@ -45,6 +44,14 @@ def set_acquire_time(atime, aperiod=None):
         Should be ``AcquireTime < AcquirePeriod`` in most cases.
         When ``aperiod`` is ``None``, then it will be set slightly
         longer than ``atime``.
+    md dict:
+        Dictionary of any additional metadata to add to this image.
+
+    View:
+
+    To view acquired image (from databroker catalog) in IPython console::
+
+        cat[-1].primary.read()["adsimdet_image"][0][0].plot.pcolormesh()
     """
     if atime <= 0:
         raise ValueError(
@@ -59,39 +66,6 @@ def set_acquire_time(atime, aperiod=None):
     logger.info("area detector (%s) acquire time: %f", adsimdet.name, atime)
     logger.info("area detector (%s) acquire period: %f", adsimdet.name, aperiod)
 
-
-def take_image(coarse=None, fine=None, md=None):
-    """
-    Take one image with the area detector.
-
-    1. Move the stages as directed.  No move if ``None``.
-    2. Gather metadata
-    3. Open the shutter
-    4. Take the image
-    5. Close the shutter
-
-    Parameters:
-
-    coarse (x, y) or ``None``:
-        Pair (tuple or list) of *new* x, y positions.
-        If ``None``, samplexy.coarse will not be moved.
-    fine (x, y) or ``None``:
-        Pair (tuple or list) of *new* x, y positions.
-        If ``None``, samplexy.fine will not be moved.
-    md dict:
-        Dictionary of any additional metadata to add to this image.
-
-    View:
-
-    To view acquired image (from databroker catalog) in IPython console::
-
-        cat[-1].primary.read()["adsimdet_image"][0][0].plot.pcolormesh()
-    """
-    if coarse is not None:
-        yield from move_coarse_positioner(coarse)
-    if fine is not None:
-        yield from move_fine_positioner(fine)
-
     _md = dict(
         plan_name = "take_image",
         incident_beam = incident_beam.get(),
@@ -99,12 +73,12 @@ def take_image(coarse=None, fine=None, md=None):
         coarse_y = samplexy.coarse.y.position,
         fine_x = samplexy.fine.x.get(),
         fine_y = samplexy.fine.y.get(),
+        acquire_time = adsimdet.cam.acquire_time.get(),
+        acquire_period = adsimdet.cam.acquire_period.get(),
     )
     _md.update(md or {})
 
-    yield from open_shutter()
     uid = yield from bp.count([adsimdet], md=_md)
-    yield from close_shutter()
 
     try:
         # write the image file name to a PV
@@ -114,6 +88,7 @@ def take_image(coarse=None, fine=None, md=None):
         yield from bps.mv(image_file_created, str(hfile))
     except Exception as exc:
         # avoid crashing the plan just for this information
+        print(f"Exception involving name of image file: {exc}")
         logger.warning("Exception involving name of image file: %s", exc)
 
     return uid
