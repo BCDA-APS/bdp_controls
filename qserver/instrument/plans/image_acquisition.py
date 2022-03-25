74d4137ba262461f3caf97e2a211c8acd52b7868
Pete R Jemian <prjemian@gmail.com>
2022-03-25 10:41:01 -0500

    MNT #10 prime the HDF5 plugin

--- qserver/instrument/plans/image_acquisition.py
+++ qserver/instrument/plans/image_acquisition.py
@@ -1,8 +1,8 @@
 """
-Plans for the BPD 2022 project demonstration
+Bluesky measurement plans for the BPD 2022 project demonstration
 """
 
-__all__ = ["take_image", ]
+__all__ = ["take_image", "prime_hdf_plugin"]
 
 import logging
 logger = logging.getLogger(__name__)
@@ -14,6 +14,7 @@ from ..devices import adsimdet
 from ..devices import image_file_created
 from ..devices import incident_beam
 from ..devices import samplexy
+from ..devices import shutter
 from ..iconfig_dict import iconfig
 from .move_positioners import move_coarse_positioner
 from .move_positioners import move_fine_positioner
@@ -25,7 +26,7 @@ import databroker
 import pathlib
 
 
-PTIME_EXTRA = 0.001
+APERIOD_EXTRA = 0.001
 cat = databroker.catalog[iconfig["DATABROKER_CATALOG"]]
 
 
@@ -58,7 +59,7 @@ def take_image(atime, aperiod=None, md=None):
             f"Improper AcquireTime value, must be >0, received {atime}"
         )
     if aperiod is None or aperiod <= atime:
-        aperiod = atime + PTIME_EXTRA
+        aperiod = atime + APERIOD_EXTRA
 
     adsimdet.cam.stage_sigs["acquire_time"] = atime
     adsimdet.cam.stage_sigs["acquire_period"] = aperiod
@@ -69,26 +70,41 @@ def take_image(atime, aperiod=None, md=None):
     _md = dict(
         plan_name = "take_image",
         incident_beam = incident_beam.get(),
+        incident_beam_units = "simulation",
         coarse_x = samplexy.coarse.x.position,
         coarse_y = samplexy.coarse.y.position,
+        coarse_x_units = samplexy.coarse.x.egu,
+        coarse_y_units = samplexy.coarse.y.egu,
         fine_x = samplexy.fine.x.get(),
         fine_y = samplexy.fine.y.get(),
-        acquire_time = adsimdet.cam.acquire_time.get(),
-        acquire_period = adsimdet.cam.acquire_period.get(),
+        acquire_time = atime,
+        acquire_period = aperiod,
+        shutter = shutter.state,
     )
     _md.update(md or {})
 
     uid = yield from bp.count([adsimdet], md=_md)
+    print(f"DIAGNOSTIC: {uid = }")
 
     try:
         # write the image file name to a PV
-        r = cat[uid].primary._get_resources()[0]
-        hfile = pathlib.Path(r["root"]) / r["resource_path"]
-        logger.info("Image file '%s' (exists: %s)", hfile, hfile.exists())
-        yield from bps.mv(image_file_created, str(hfile))
+        run = cat[uid]
+        print(f"DIAGNOSTIC: {run = }")
+        r = run.primary._get_resources()[0]
+        print(f"DIAGNOSTIC: {r = }")
+        hdffile = pathlib.Path(r["root"]) / r["resource_path"]
+        print(f"DIAGNOSTIC: {hdffile = },  {hdffile.exists()=}")
+        logger.info("Image file '%s' (exists: %s)", hdffile, hdffile.exists())
+        yield from bps.mv(image_file_created, str(hdffile))
     except Exception as exc:
         # avoid crashing the plan just for this information
         print(f"Exception involving name of image file: {exc}")
         logger.warning("Exception involving name of image file: %s", exc)
 
     return uid
+
+
+def prime_hdf_plugin():
+    """Prime HDF file plugin by pushing one frame to it."""
+    adsimdet.hdf1.warmup()
+    yield from bps.null()
