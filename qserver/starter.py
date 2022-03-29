47f015f1c5c281d9914d713c1602e485a419ae2c
Pete R Jemian <prjemian@gmail.com>
2022-03-28 21:14:39 -0500

    MNT #10 reporting

--- qserver/starter.py
+++ qserver/starter.py
@@ -10,40 +10,48 @@ sys.path.append(
 
 from instrument import iconfig
 from instrument.qserver import *
+from ophyd import Device, Signal
+import inspect
 import pyRestTable
-
-devices = (
-    ad_x_calc,
-    ad_y_calc,
-    adsimdet,
-    gp_stats,
-    image_file_created,
-    incident_beam_calc,
-    incident_beam,
-    samplexy,
-    shutter,
-)
 
 
 def startup_report():
+    """Show instrument data on startup"""
     table = pyRestTable.Table()
     table.labels = "key value".split()
     for k, v in sorted(iconfig.items()):
         table.addRow((k, v))
-    print("instrument configuration (iconfig):")
-    print(table)
+    if len(table.rows) > 0:
+        print("")
+        print("Instrument configuration (iconfig):")
+        print(table)
+
+    glo = globals().copy()
 
     table = pyRestTable.Table()
-    table.labels = "device/object connected? prefix/pvname".split()
-    for _obj in devices:
-        _obj.wait_for_connection()
-        p = ""
-        for aname in "pvname prefix".split():
-            if hasattr(_obj, aname):
-                p = getattr(_obj, aname)
-        table.addRow((_obj.name, _obj.connected, p))
-    print("Devices and signals:")
-    print(table)
+    table.labels = "device/object pvprefix/pvname connected?".split()
+    for k, v in sorted(glo.items()):
+        if isinstance(v, (Device, Signal)) and not k.startswith("_"):
+            v.wait_for_connection()
+            p = ""
+            for aname in "pvname prefix".split():
+                if hasattr(v, aname):
+                    p = getattr(v, aname)
+            table.addRow((v.name, p, v.connected))
+    if len(table.rows) > 0:
+        print("Table of Devices and signals:")
+        print(table)
+
+    plans = [
+        k
+        for k, v in sorted(glo.items()) 
+        if inspect.isgeneratorfunction(v)
+    ]
+    if len(plans) > 0:
+        print("List of Plans:")
+        for k in plans:
+            print(f"* {k}{inspect.signature(glo[k])}")
+        print("")
 
 
 startup_report()
