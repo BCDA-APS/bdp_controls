f9e602ed7bb46cfb68995a4f0582d7656e64a588
Pete Jemian <prjemian@gmail.com>
2022-04-06 18:57:09 -0500

    PKG #23 for tiled

--- tiled/create_env.sh
+++ tiled/create_env.sh
@@ -12,6 +12,65 @@ conda create -y \
     matplotlib \
     pyresttable \
     spec2nexus \
+    tzlocal \
+    Mako \
+    MarkupSafe \
+    aiofiles \
+    alembic \
+    anyio \
+    appdirs \
+    asgiref \
+    blosc \
+    cachetools \
+    cachey \
+    click \
+    cloudpickle \
+    dask \
+    ecdsa \
+    entrypoints \
+    et-xmlfile \
+    fastapi \
+    fsspec \
+    greenlet \
+    h11 \
+    h5netcdf \
+    heapdict \
+    httpcore \
+    httptools \
+    httpx \ \
+    jinja2 \
+    jmespath \
+    locket \
+    lz4 \
+    msgpack \
+    openpyxl \
+    orjson \
+    pandas \
+    partd \
+    prometheus-client \
+    psutil \
+    pyarrow \
+    pyasn1 \
+    pydantic \
+    python-dotenv \
+    python-jose \
+    python-multipart \
+    pytz \
+    pyyaml \
+    rfc3986 \
+    rsa \
+    sniffio \
+    sqlalchemy \
+    starlette \
+    tifffile \
+    toolz \
+    typer \
+    typing-extensions \
+    uvicorn \
+    uvloop \
+    watchgod \
+    websockets \
+    xarray \
     -c defaults -c conda-forge
 
 conda activate "${ENV_NAME}"
