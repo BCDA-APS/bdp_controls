66e30af3240f0b1d6c61494dd2044086c84105ee
Beamline Data Pipeline - Roger Sersted <bdp@terrier.xray.aps.anl.gov>
2022-04-25 10:01:39 -0500

    remove notice: multiple IOCs detected ...

--- qserver/bash_epics_env_config.sh
+++ qserver/bash_epics_env_config.sh
@@ -7,7 +7,8 @@
 #     Warning: "Identical process variable names on multiple servers"
 
 LOCALHOST=127.0.0.1
+HOST_IP=$(ifconfig vlan0534 | grep " inet " | awk  '{print $2}')
 S1_PV_GATEWAY_IP=164.54.112.168
 
 export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_ADDR_LIST="${LOCALHOST} ${S1_PV_GATEWAY_IP}"
+export EPICS_CA_ADDR_LIST="${LOCALHOST} ${HOST_IP} ${S1_PV_GATEWAY_IP}"
