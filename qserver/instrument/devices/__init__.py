ce659cabec9ccb96ca9fc3fa232c09c1860bc71a
Beamline Data Pipeline - Roger Sersted <bdp@terrier.xray.aps.anl.gov>
2022-07-15 16:08:04 -0500

    MNT #52 rename

--- qserver/instrument/devices/__init__.py
+++ qserver/instrument/devices/__init__.py
@@ -6,13 +6,13 @@ print(__file__)
 
 # define the devices
 from .aps_source import *
-# from .calculation_records import *
-# from .feedback_api import *
+from .calculation_records import *
+from .feedback_api import *
 from .image_file_signal import *
-# from .ioc_stats import *
-# from .samplexy_stage import *
-# from .simulated_beam import *
-# from .simulated_shutter import *
+from .ioc_stats import *
+from .samplexy_stage import *
+from .simulated_beam import *
+from .simulated_shutter import *
 
-# # area detector comes after previous devices are defined
-# from .area_detector import *
+# area detector comes after previous devices are defined
+from .ad_simdet import *
