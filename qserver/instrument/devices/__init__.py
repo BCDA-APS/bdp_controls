69a236f7e4782f7c5967527051def60970449f8e
Beamline Data Pipeline - Roger Sersted <bdp@terrier.xray.aps.anl.gov>
2022-07-15 15:51:09 -0500

    MNT #52 disable M4 demo parts for now

--- qserver/instrument/devices/__init__.py
+++ qserver/instrument/devices/__init__.py
@@ -2,14 +2,17 @@
 Ophyd Devices for Bluesky measurements.
 """
 
+print(__file__)
+
 # define the devices
 from .aps_source import *
-from .calculation_records import *
-from .feedback_api import *
-from .ioc_stats import *
-from .samplexy_stage import *
-from .simulated_beam import *
-from .simulated_shutter import *
+# from .calculation_records import *
+# from .feedback_api import *
+from .image_file_signal import *
+# from .ioc_stats import *
+# from .samplexy_stage import *
+# from .simulated_beam import *
+# from .simulated_shutter import *
 
-# area detector comes after previous devices are defined
-from .area_detector import *
+# # area detector comes after previous devices are defined
+# from .area_detector import *
--- qserver/instrument/plans/__init__.py
+++ qserver/instrument/plans/__init__.py
@@ -4,10 +4,10 @@ Bluesky measurement & support plans.
 
 from bluesky.plan_stubs import sleep
 from .adjust_scan_id import *
-from .game import *
-from .image_acquisition import *
-from .metadata_support import *
-from .move_positioners import *
-from .print_information import *
-from .shutter_controls import *
-from .trajectories import *
+# from .game import *
+# from .image_acquisition import *
+# from .metadata_support import *
+# from .move_positioners import *
+# from .print_information import *
+# from .shutter_controls import *
+# from .trajectories import *
