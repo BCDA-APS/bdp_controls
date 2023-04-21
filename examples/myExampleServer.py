093ac022b00675a9b146c42b48ff05c5322010bd
Pete R Jemian <prjemian@gmail.com>
2023-04-21 17:12:33 -0500

    MNT focus the name

--- examples/myExampleServer.py
+++ examples/myExampleServer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Demo a custom implementation of the bdpPvaMetadata Example.
+Demo a custom server using the bdpPvaMetadata Example.
 
 Add minimal effort in each part to be customized.  Just for demo.
 """
@@ -11,23 +11,29 @@ import bdpPvaMetadata
 
 DEFAULT_CHANNEL = "Bdp1:Handshake"
 
+
 class MyServer(bdpPvaMetadata.Server):
     def getValue(self):
         return {"example": 1.0}
 
-class MyListener(bdpPvaMetadata.Listener):
-    def monitor(self, pv_object):
-        print(self.getValue(pv_object))
+
+# example only
+# class MyListener(bdpPvaMetadata.Listener):
+#     def monitor(self, pv_object):
+#         print(self.getValue(pv_object))
+
 
 def run_server(duration=10, period=1):
     channel = DEFAULT_CHANNEL
 
     server = MyServer(channel)
+    server.start()
     deadline = time.time() + duration
     while time.time() < deadline:
         server.publishContent(server.getValue())
         time.sleep(period)
     server.stop()
+
 
 if __name__ == "__main__":
     run_server()
